478025b4d560b124f59029a46d2892d47a8b8dc3
Felipe Carvalho <felipe_carvalho0@hotmail.com>
2023-06-02 13:55:51 -0300

    feat: new exe

--- aula12exb/script.js
+++ aula12exb/script.js
@@ -5,13 +5,29 @@ function verificar() {
     var res = window.document.getElementById("text")
     var sex = window.document.getElementsByName("radsex")
     var sexo = ''
+    var foto = window.document.getElementById("foto")
     res.style.textAlign = 'center'
-    
-    if (Number(ano.value) > data.getFullYear()) {
-        window.alert("ERRO")
+    var img = window.document.createElement('img')
+    img.setAttribute('id', 'img')
+    foto.style.textAlign = 'center'
+    foto.style.padding = '10px 0px '
+
+
+    if (Number(ano.value) > data.getFullYear() || ano.value.length == 0) {
+        res.innerHTML = 'Informe um número válido.'
     } else if (sex[0].checked) {
         sexo = 'Homem'
+        if (idade >= 0 && idade <= 10) {
+            img.src = 'img/pexels-victoria-rain-1648377.jpg'
+        } else if (idade > 10 && idade <= 18) {
+            img.src = 
+        } else if (idade > 18 && idade < 60 ) {
+            img.src = 
+        } else {
+            img.src = 
+        }
         res.innerHTML = `Detectamos ${sexo} com ${idade} anos.`
+        foto.appendChild(img)
     } else if (sex[1].checked) {
         sexo = 'Mulher'
         res.innerHTML = `Detectamos ${sexo} com ${idade} anos.`
