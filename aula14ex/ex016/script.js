bccd6d575671aa38a6b9756a1b6a765ef6f3e2eb
Felipe Carvalho <felipe_carvalho0@hotmail.com>
2023-06-04 14:34:56 -0300

    refactor:

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -2,12 +2,18 @@ function verificar() {
     var inicio = window.document.getElementById("inicio")
     var fim = window.document.getElementById('fim')
     var passo = window.document.getElementById("passo")
+    var res1 = window.document.querySelector("#text")
     var res = window.document.querySelector("#text1")
     res.innerHTML = ''
 
-    var c = Number(inicio.value)
-    while ( c <= Number(fim.value)) {
-        res.innerHTML += `${c} `
-        c += Number(passo.value)
+    if (Number(inicio.value) > Number(fim.value) || Number(passo.value) > Number(fim.value)) {
+        res.innerHTML = 'Informe um valor correto.'
+    } else {
+        res1.innerHTML = 'Contando...'
+        var c = Number(inicio.value)
+        while ( c <= Number(fim.value)) {
+            res.innerHTML += `${c} `
+            c += Number(passo.value)
+        }
     }
 }
