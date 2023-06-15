c409f7f790ce452f6043d665fccc361da8857118
Felipe Carvalho <felipe_carvalho0@hotmail.com>
2023-06-15 13:11:01 -0300

    feat: new exe

---
+++ Curso-Udemy/aula003.js
@@ -0,0 +1,15 @@
+// Array Básico
+let array = ["Luis", "Felipe", "Andre", "Pedro"]
+// Alterando valores do nosso array através dos índices.
+array[0] = 'Pedro'
+// Adicionando um valor a posição final do array.
+array[array.length] = "Lucas"
+// Adicionando um valor a posição final do array.
+array.push('Jonas')
+// Adicionando um valor a posição inicial do array.
+array.unshift('Tales')
+// Exibindo a quantidade de elementos do array.
+console.log(array.length)
+// Exibindo o elemento na posição de elementos do array.
+console.log(array[0])
+console.log(array)
