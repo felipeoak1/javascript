7b8678b81b5faff93e48e7b9e5793d33c2879a7f
Felipe Carvalho <felipe_carvalho0@hotmail.com>
2023-06-19 18:18:06 -0300

    feat: new exe

--- Curso-Udemy/exercicios/ex005/script.js
+++ Curso-Udemy/exercicios/ex005/script.js
@@ -11,3 +11,6 @@ let dias_da_semana = ['Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-f
 let meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
 
 res.innerHTML = `${dias_da_semana[dia_semana]}, ${dia_mes} de ${meses[mes]} de ${ano}. Hora: ${hora}:${minutos}`
+
+// Mas podemos utilizar o seguinte código para obter o mesmo resultado apresentado acima.
+// res.innerHTML += Intl.DateTimeFormat('pt-BR', { dateStyle: "full", timeStyle: "medium" }).format(new Date())
