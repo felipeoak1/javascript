d39535dcbf89f76d30cdb58e23a44bbc284a097d
Felipe Carvalho <felipe_carvalho0@hotmail.com>
2023-06-19 12:55:53 -0300

    refactor: add event listener instead html events

--- Curso-Udemy/exercicios/ex004/script.js
+++ Curso-Udemy/exercicios/ex004/script.js
@@ -5,6 +5,9 @@ function enviarFormulario(event) {
     // ou realizar outras ações desejadas
 
   }
+  
+let submit = document.getElementById("submit")
+submit.addEventListener("click", calcular)
 
 function calcular() {
     let altura = document.getElementById('ialtura').value
@@ -12,19 +15,26 @@ function calcular() {
     let res = document.getElementById("res")
     let imc = Number.parseFloat(peso) / (Number.parseFloat(altura) ** 2)
     
+    if (altura.length == 0 || peso.length == 0 ) {
+        
+        res.innerHTML = '<p style="background-color:red">Forneça um valor válido.</p>'
 
-    if (imc <= 18.5) {
-        res.innerHTML = `<p>Seu IMC é de ${imc.toFixed(2)} (Abaixo do peso)</p>`
-    } else if ( imc <= 24,9) {
-        res.innerHTML = `<p>Seu IMC é de ${imc.toFixed(2)} (Peso normal)</p>`
-    } else if ( imc <= 29,9) {
-        res.innerHTML = `<p>Seu IMC é de ${imc.toFixed(2)} (Sobrepeso)</p>`
-    } else if ( imc <= 34,9) {
-        res.innerHTML = `<p>Seu IMC é de ${imc.toFixed(2)} (Obesidade grau 1)</p>`
-    } else if ( imc < 39,9) {
-        res.innerHTML = `<p>Seu IMC é de ${imc.toFixed(2)} (Obesidade grau 2)</p>`
     } else {
-        res.innerHTML = `<p>Seu IMC é de ${imc.toFixed(2)} (Obesidade grau 3)</p>`
+
+        if (imc <= 18.5) {
+            console.log(altura.length)
+            res.innerHTML = `<p>Seu IMC é de ${imc.toFixed(2)} (Abaixo do peso)</p>`
+        } else if ( imc <= 24,9) {
+            res.innerHTML = `<p>Seu IMC é de ${imc.toFixed(2)} (Peso normal)</p>`
+        } else if ( imc <= 29,9) {
+            res.innerHTML = `<p>Seu IMC é de ${imc.toFixed(2)} (Sobrepeso)</p>`
+        } else if ( imc <= 34,9) {
+            res.innerHTML = `<p>Seu IMC é de ${imc.toFixed(2)} (Obesidade grau 1)</p>`
+        } else if ( imc < 39,9) {
+            res.innerHTML = `<p>Seu IMC é de ${imc.toFixed(2)} (Obesidade grau 2)</p>`
+        } else {
+            res.innerHTML = `<p>Seu IMC é de ${imc.toFixed(2)} (Obesidade grau 3)</p>`
+        }
     }
 
 }
