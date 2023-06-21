131c994b3c06b04c93056e89ff5e8856cb9dd83a
Felipe Carvalho <felipe_carvalho0@hotmail.com>
2023-06-21 15:06:43 -0300

    feat:new exe

---
+++ Curso-Udemy/aula011.js
@@ -0,0 +1,3 @@
+for (let c = 0; c < 10; c += 5) {
+    console.log(c)
+}
---
+++ Curso-Udemy/exercicios/ex006/script.js
@@ -0,0 +1,16 @@
+let section = document.querySelector("#container")
+const elementos = [
+    {tag: 'p', texto: 'frase 1'},
+    {tag: 'p', texto: 'frase 2'},
+    {tag: 'p', texto: 'frase 3'},
+    {tag: 'p', texto: 'frase 4'},
+    {tag: 'p', texto:'frase 5'},
+]
+
+for (let c = 0; c < elementos.length; c++) {
+    let { tag, texto } = elementos[c]
+    let element = document.createElement(tag)
+    element.setAttribute('id', c)
+    element.innerText = texto
+    section.appendChild(element)
+}
