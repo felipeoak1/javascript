9cdeadc6e2c55089875f5a216e02ae57851b2e33
Felipe Carvalho <felipe_carvalho0@hotmail.com>
2023-06-24 21:12:05 -0300

    feat: new functions

--- Curso-Udemy/exercicios/ex008/script.js
+++ Curso-Udemy/exercicios/ex008/script.js
@@ -3,53 +3,63 @@ const iniciar = document.getElementById('iniciar')
 const pausar = document.getElementById('pausar')
 const zerar = document.getElementById('zerar')
 
+let controle;
 let seconds = 0
 let minutes = 0
 let hours = 0
 
-let inicio = () => {
-
-    if (seconds == 59) {
-        seconds = 0
-        minutes++
-    }
-
-    if (minutes == 59) {
-        minutes = 0
-        hours++
-    }
-
-    if (hours == 24) {
-        hours = 0
-    }
-
-    seconds++
-    relogio.innerHTML = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
-}
-
 function start () {
+
+    if (controle) {
+        return
+    }
+
+    let inicio = () => {
+
+        if (seconds == 59) {
+            seconds = 0
+            minutes++
+        }
+
+        if (minutes == 59) {
+            minutes = 0
+            hours++
+        }
+
+        if (hours == 24) {
+            hours = 0
+        }
+
+        seconds++
+        controle = true
+        relogio.innerHTML = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+    }
+
     relogio.style.color = 'black'
     let l1 = setInterval(
         inicio, 1000
     )
+
+    let pause = () => {
+        controle = false
+        relogio.style.color = 'red'
+        setTimeout(()=>{clearInterval(l1)}, 1)
+    }
+
+    pausar.addEventListener('click', pause)
 }
 
-function pause () {
-    relogio.style.color = 'red'
-    
-}
 
 function reset () {
     seconds = 0
     minutes = 0
     hours = 0   
-      
+    
+    relogio.style.color = 'black'
     relogio.innerHTML = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+
 }
 
 
 iniciar.addEventListener('click', start)
-
-pausar.addEventListener('click', pause)
-
 zerar.addEventListener('click', reset)
