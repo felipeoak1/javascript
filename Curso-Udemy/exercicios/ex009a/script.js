068eb895137c5e3fd5406360d732dae2734652d3
Felipe Carvalho <felipe_carvalho0@hotmail.com>
2023-07-01 14:49:14 -0300

    feat: new exe practice

--- Curso-Udemy/exercicios/ex009a/script.js
+++ Curso-Udemy/exercicios/ex009a/script.js
@@ -1,7 +1,6 @@
 let tasks = document.getElementById('itarefas')
 let ul = document.getElementById('lista')
 let add = document.getElementById('botaoAdicionar')
-let taskList = []
 
 add.addEventListener('click', addTask)
 
@@ -9,13 +8,23 @@ function addTask() {
     let li = document.createElement('li')
     let del = document.createElement('button')
     let div = document.createElement('div')
+    let span = document.createElement('span')
+
     del.setAttribute('class', 'botaodelete')
     div.setAttribute('class', 'divbotao')
+    span.setAttribute('class', 'material-symbols-outlined')
+    del.addEventListener('click', deleteTask)
 
-    taskList.push(tasks.value)
+    span.innerHTML = 'delete'
     li.innerHTML = tasks.value
+
+    del.appendChild(span)
     div.appendChild(li)
     div.appendChild(del)
     ul.appendChild(div)
     
+}
+
+function deleteTask() {
+    this.parentElement.remove()
 }
