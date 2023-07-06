8824130f3fa4813a9319dd80b63b4226a9b75400
Felipe Carvalho <felipe_carvalho0@hotmail.com>
2023-07-05 22:35:50 -0300

    feat: new exe

--- Curso-Udemy/exercicios/ex000/ex000.js
+++ Curso-Udemy/exercicios/ex000/ex000.js
@@ -51,5 +51,11 @@ let maxNumber = (n, n1) => n > n1 ? n : n1
 
 console.log(maxNumber(23, 6))
 
+// Write a JavaScript program to get the website URL (loading page). 
+
+console.log(document.URL())
+
+// Write a JavaScript program to get the difference between a given number and 13, if the number is broader than 13 return double the absolute difference. 
+
 
 
