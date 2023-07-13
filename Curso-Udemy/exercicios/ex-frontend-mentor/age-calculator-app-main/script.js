1f7a71a2c97d6473c175978f38b84afe39414ad5
Felipe Carvalho <felipe_carvalho0@hotmail.com>
2023-07-13 15:24:10 -0300

    feat: fix calc days and months

--- Curso-Udemy/exercicios/ex-frontend-mentor/age-calculator-app-main/script.js
+++ Curso-Udemy/exercicios/ex-frontend-mentor/age-calculator-app-main/script.js
@@ -1,5 +1,7 @@
 let date = new Date()
 let currentYear = date.getFullYear()
+let currentMonth = date.getMonth()
+let currentDay = date.getDate()
 
 let day = document.getElementById("iday")
 let month = document.getElementById("imonth")
@@ -11,5 +13,27 @@ function calcAge() {
     let yearText = document.getElementById("yeartext")
     let age = currentYear - Number(year.value)
     yearText.innerHTML = age
+    calcMonths()
+    calcDays()
 }
 
+function calcMonths() {
+    let monthAge;
+    let monthText = document.getElementById("monthtext")
+    
+    if (month.value > currentMonth + 1) {
+        monthAge = Number(month.value) - (currentMonth + 1)
+    } else {
+        monthAge = (currentMonth + 1 ) - Number(month.value)
+    }
+
+    monthText.innerHTML = `${monthAge.toString().padStart(2, '0')}`
+    
+}
+
+function calcDays() {
+    let dayText = document.getElementById("daytext")
+    let dayNow = new Date().getDate()
+
+    dayText.innerHTML = `${dayNow.toString().padStart(2, '0')}`
+}
