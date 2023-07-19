63c1f594a34aa5241a219096a4a6244618b68734
Felipe Carvalho <felipe_carvalho0@hotmail.com>
2023-07-19 14:59:13 -0300

    feat: new check inputs func

--- Curso-Udemy/exercicios/ex-frontend-mentor/age-calculator-app-main/assets/script/script.js
+++ Curso-Udemy/exercicios/ex-frontend-mentor/age-calculator-app-main/assets/script/script.js
@@ -1,10 +1,15 @@
+// Date variables
 let date = new Date()
 let currentYear = date.getFullYear()
 let currentMonth = date.getMonth()
 let currentDay = date.getDate()
+
+// Control variables
+let yearChoosed;
 let hrActive;
 let control = true
 
+// HTML Elements
 let day = document.getElementById("iday")
 let month = document.getElementById("imonth")
 let year = document.getElementById("iyear")
@@ -15,20 +20,22 @@ let button = document.getElementById("button")
 let imgButton = document.getElementById("img-button")
 button.addEventListener('click', checkInputs)
 button.addEventListener("mouseenter", changeColor)
-button.addEventListener("mouseleave", colorButton)
-
-function colorButton() {
-    imgButton.style.backgroundColor = 'hsl(259, 100%, 65%)'
-    button.style.backgroundColor = 'hsl(259, 100%, 65%)'
-}
+button.addEventListener("mouseleave", mainColor)
 
 function changeColor() {
     imgButton.style.backgroundColor = 'black'
     button.style.backgroundColor = 'black'
 }
 
-function checkWidth() {
 
+function mainColor() {
+    imgButton.style.backgroundColor = 'hsl(259, 100%, 65%)'
+    button.style.backgroundColor = 'hsl(259, 100%, 65%)'
+}
+
+
+function checkWidth() {
+    
     if (window.innerWidth > 375) {
         let hr = document.getElementById("hr2")
         hr.style.display = 'none'
@@ -42,37 +49,44 @@ function checkWidth() {
     }
 }
 
-function calcAge() {
-    let yearText = document.getElementById("yeartext")
-    let age = currentYear - Number(year.value)
-    yearText.innerHTML = age
+function NumberInputs(element, text) {
 
-    calcMonths()
-    calcDays()
+    if (element.parentElement.childNodes.length == 5){
+        let p = document.createElement('p')
+        p.setAttribute('class', 'pcheckinput')
+        element.parentElement.appendChild(p).innerHTML = text
+    } else {
+        element.parentElement.childNodes[5].innerHTML = text
+    }
+
+    element.style.border = 'solid 1px hsl(0, 100%, 67%)'
+    document.getElementById(`i${element.name.toString().toLowerCase()}label`).style.color = 'hsl(0, 100%, 67%)'
+    control = false
 }
 
-function checkNumberInputs(element, number) {
-    console.log(element)
-    if (Number(element.value) > number) {
-        if (element.parentElement.childNodes.length == 5){
-            let p = document.createElement('p')
-            p.setAttribute('class', 'pcheckinput')
-            element.parentElement.appendChild(p).innerHTML = `Must be a valid ${element.name}.`
-        } else {
-            element.parentElement.childNodes[5].innerHTML = `Must be a valid ${element.name}.`
-        } 
 
-        element.style.border = 'solid 1px hsl(0, 100%, 67%)'
-        document.getElementById(`i${element.name.toString().toLowerCase()}label`).style.color = 'hsl(0, 100%, 67%)'
-        control = false
+function checkNumberInputs(element, number) {
+
+    if (element.name == "Year") {
+        yearChoosed = Number(element.value)
+    }
+
+    if (element.name == "Month" && yearChoosed == date.getFullYear()) {
+        if (Number(element.value) > date.getMonth() + 1) {
+            NumberInputs(element, `Must be in the past.`)
+        }
+    }
+
+    if (Number(element.value) > number || Number(element.value) <= 0) {
+        NumberInputs(element, `Must be a valid ${element.name}.`)
     }
 }
 
 function checkInputs() {
 
+    checkNumberInputs(year, date.getFullYear())
     checkNumberInputs(day, 31)
     checkNumberInputs(month, 12)
-    checkNumberInputs(year, date.getFullYear())
 
     list.map(
         (e)=>{ 
@@ -117,16 +131,29 @@ function checkInputs() {
         )
 
     // Back all configurations to normal after one second and half
-    setTimeout((e) => {
+    setTimeout(() => {
         list.map((e)=>{
             e.removeAttribute('disabled')
-            e.parentElement.childNodes[5].innerHTML = ' '
             e.style.border = "solid 1px rgba(0, 0, 0, 0.253)"
             Array.from(document.getElementsByTagName('label')).map((e)=>{e.style.color = 'hsl(0, 1%, 44%)'})
+            if (e.parentElement.childNodes.length == 6) {
+                e.parentElement.childNodes[5].innerHTML = ' '
+            }
             control = true
             })
-    }, 1500);
+    }, 1000);
 }
+
+
+function calcAge() {
+    let yearText = document.getElementById("yeartext")
+    let age = currentYear - Number(year.value)
+    yearText.innerHTML = age
+
+    calcMonths()
+    calcDays()
+}
+
 
 function calcMonths() {
     let monthAge;
@@ -140,6 +167,7 @@ function calcMonths() {
 
     monthText.innerHTML = `${monthAge.toString().padStart(2, '0')}`
 }
+
 
 function calcDays() {
     let dayText = document.getElementById("daytext")
