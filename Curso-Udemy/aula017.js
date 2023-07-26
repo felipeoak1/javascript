8ba4b446441bfb1962c74bff8373531ba319f399
Felipe Carvalho <felipe_carvalho0@hotmail.com>
2023-07-26 15:33:22 -0300

    feat: new exe

--- Curso-Udemy/aula017.js
+++ Curso-Udemy/aula017.js
@@ -22,3 +22,9 @@ nomes.splice(nomes.length, 0, 'Pedro')
 nomes.splice(-1, 1)
 
 /* Concatenação de Arrays */
+
+const a1 = [1, 2, 3]
+const a2 = [4, 5, 6]
+const a3 = a1.concat(a2, [7, 8, 9], 'Luiz')
+const a4 = [...a1,"Pedro", ...a2]
+console.log(a4)
