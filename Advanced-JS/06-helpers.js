429f7a7cb26e3a4042b5df2a46bb773f1dea4819
Felipe Carvalho <felipe_carvalho0@hotmail.com>
2023-08-11 23:21:39 -0300

    feat: new exe

---
+++ Advanced-JS/06-helpers.js
@@ -0,0 +1,16 @@
+const tech = 'Node.js'
+
+const array = ['Node.Js', 'React', 'TypeScript']
+
+const includesString = tech.includes('Node.') // Case Sensitive
+
+const includesArray = array.includes('React') // Procurando pelo valor exato no array. 
+
+// Startswith e Endswith
+
+const startsWith = tech.startsWith('Node')
+
+const endsWith = tech.endsWith('ode.js')
+
+console.log(startsWith) 
+console.log(endsWith) 
---
+++ Advanced-JS/modules/sayHello.js
@@ -0,0 +1,11 @@
+function sayHello(name) {
+    console.log(name)
+}
+
+const userName = 'Mateus'
+
+// Ou
+
+export {sayHello} // Aplicando uma desestruturação obrigatória na importação.
+
+export default userName
