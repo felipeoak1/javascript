c2a0f9b0942c4a4049751f6ede427ae61a8d9c2f
Felipe Carvalho <felipe_carvalho0@hotmail.com>
2023-10-29 17:23:08 -0300

    feat: Using contextApi with class component

--- ReactThemeTeste/src/components/Footer/index.js
+++ ReactThemeTeste/src/components/Footer/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { ThemeContext } from '../../contexts/ThemeContext';
 import PropTypes from 'prop-types'
 import { Container } from './styles';
 
-export default function Footer(props) {
+export default function Footer() {
+  const {theme, handleToggleTheme} = useContext(ThemeContext)
   return (
     <Container>
       <span>JStack's Blog. Todos os direitos reservados.</span>
-      <button type="button" onClick={props.changeColor}>{props.tema == 'dark' ? '🌞': '🌚'}</button>
+      <button type="button" onClick={handleToggleTheme}>{theme == 'dark' ? '🌞': '🌚'}</button>
     </Container>
   );
 }
 
-Footer.propTypes = {
-  changeColor: PropTypes.func.isRequired,
-  tema: PropTypes.string.isRequired,
-}
+// Footer.propTypes = {
+//   changeColor: PropTypes.func.isRequired,
+//   tema: PropTypes.string.isRequired,
+// }
--- ReactThemeTeste/src/components/Header/index.js
+++ ReactThemeTeste/src/components/Header/index.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types'
+import {ThemeContext} from '../../contexts/ThemeContext'
 
 import { Container } from './styles';
 
 export default class Header extends React.Component {
   
   render() {
-    const {changeColor} = this.props
-    const {tema} = this.props
+    // const {changeColor} = this.props
+    // const {tema} = this.props
     
     return ( 
-      <Container>
-        <h1>JStack's Blog</h1>
-        <button type="button" onClick={()=>changeColor()}>{tema == 'dark' ? '🌞' : '🌑'}</button>
-      </Container> 
+      <ThemeContext.Consumer>
+        {({theme, handleToggleTheme})=>(
+          <Container>
+            <h1>JStack's Blog</h1>
+            <button type="button" onClick={()=>handleToggleTheme()}>{theme == 'dark' ? '🌞' : '🌑'}</button>
+          </Container> 
+        )}
+      </ThemeContext.Consumer>
+
     )
   }
 }
 
-Header.propTypes = {
-  changeColor: PropTypes.func.isRequired,
-  tema: PropTypes.string.isRequired,
-}
+// Se não há props não há necessidade de propTypes.
+// Header.propTypes = {
+//   changeColor: PropTypes.func.isRequired,
+//   tema: PropTypes.string.isRequired,
+// }
